57cb22a480fe683cb01cbd2baace3531f7dc1aae
Ying Fan <yingcathyfan@gmail.com>
2018-01-30 11:37:41 -0800

    replace local db to mLab MongoDB

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my-blog');
+//mongoose.connect('mongodb://localhost/my-blog');
+mongoose.connect('mongodb://heroku_mfmb8z03:r42mpfpt307o31d13vjli7dmag@ds139619.mlab.com:39619/heroku_mfmb8z03');
 mongoose.Promise = Promise;
 
 const app = express();
